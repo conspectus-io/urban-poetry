8079478624acb41aa94e240c7a320361040e4269
ev-dev <evan.reed1100@gmail.com>
2017-05-01 19:16:39 -0400

    testing jquery events

--- testing/mad-lib-api-test.js
+++ testing/mad-lib-api-test.js
@@ -3,6 +3,9 @@ const $ = require('jquery');
 
 function getData() {
     $(function () {
+        $("body").on("click", function() {
+            $("body").prepend("<p>working test example...</p>");
+        }
         $.ajax({
             url: "metaphorpsum.com/sentences/4",
             success: function (data) {
