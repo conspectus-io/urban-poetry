6f4aff412827e540f522d41c15cc507eb193649d
ev-dev <evan.reed1100@gmail.com>
2017-05-01 19:18:55 -0400

    testing jquery events 2

--- testing/mad-lib-api-test.js
+++ testing/mad-lib-api-test.js
@@ -1,18 +1,14 @@
 const $ = require('jquery');
 
-
-function getData() {
-    $(function () {
-        $("body").on("click", function() {
-            $("body").prepend("<p>working test example...</p>");
+$(function () {
+    $("body").on("click", function() {
+        $("body").prepend("<p>working test example...</p>");
+    });
+    
+    $.ajax({
+        url: "metaphorpsum.com/sentences/4",
+        success: function (data) {
+            console.log(data);
         }
-        $.ajax({
-            url: "metaphorpsum.com/sentences/4",
-            success: function (data) {
-                console.log(data);
-            }
-        });
-    })
-}
-
-getData();
+    });
+});
